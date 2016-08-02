c7b444266156f1d2a8c5aae4e39911892bc870c9
hrtejada <hr.tejada121@gmail.com>
2016-08-01 19:10:24 -0600

    CFG Builder - If statements done!

--- src/main/java/com/code/graph/Grapher/startNode.java
+++ src/main/java/com/code/graph/Grapher/startNode.java
@@ -5,7 +5,7 @@ import com.github.javaparser.ast.stmt.BlockStmt;
 /**
  * Created by Beto on 7/3/16.
  */
-public class startNode extends Node{
+class startNode extends Node{
     //private int startLine;
     private String cfgName;
 
